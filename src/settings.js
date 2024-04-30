68da9733f81835d463c95849d8977d55a0c4b034
ahvo <ashley.vo1210@gmail.com>
2024-04-30 13:47:47 -0600

    install agora

---
+++ src/settings.js
@@ -0,0 +1,5 @@
+
+
+const appId = "8db98c0733964b0a9da023f8fd9b25e3"
+const token = "007eJxTYPiy9U8L+3POw9o+6jOZDJKFPlrEGF5VMOwO8I4063wzZb0Cg0VKkqVFsoG5sbGlmUmSQaJlSqKBkXGaRVqKZZKRaaoxq6dhWkMgI0PJkvUMjFAI4nMz5CZm5jlnJOblpeYwMAAAknYf0g=="
+
