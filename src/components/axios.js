bc04c569f8c423c65fb45bba219279969ad89e2a
ahvo <ashley.vo1210@gmail.com>
2024-04-30 11:15:47 -0600

    test

--- src/components/axios.js
+++ src/components/axios.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 
 const instance = axios.create ({
     baseURL: "https://letter-out-backend.onrender.com"
-    //process.env.REACT_APP_BACKEND_URL
 })
 
 export default instance
