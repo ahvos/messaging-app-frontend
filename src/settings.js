e4a16d5a7a5665e41542482a0bcf0baac8c4fa3a
ahvo <ashley.vo1210@gmail.com>
2024-04-30 13:50:43 -0600

    setting.js

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,9 @@
-
+import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "8db98c0733964b0a9da023f8fd9b25e3"
 const token = "007eJxTYPiy9U8L+3POw9o+6jOZDJKFPlrEGF5VMOwO8I4063wzZb0Cg0VKkqVFsoG5sbGlmUmSQaJlSqKBkXGaRVqKZZKRaaoxq6dhWkMgI0PJkvUMjFAI4nMz5CZm5jlnJOblpeYwMAAAknYf0g=="
 
+export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
+export const useClient = createClient(config);
+export const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
+export const channelName = "mainChannel";
